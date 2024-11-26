78fff8351696807be3db0a4a401ae2f51314693c
Guillen <guillen@email.com>
2024-11-25 21:07:51 -0300

    boa pa nois

--- frontend/src/app/cadastro/cadastro.component.ts
+++ frontend/src/app/cadastro/cadastro.component.ts
@@ -1,12 +1,44 @@
 import { Component } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-cadastro',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './cadastro.component.html',
   styleUrl: './cadastro.component.css'
 })
 export class CadastroComponent {
+  nomecompleto = String;
+  nomesocial = String;
+  nomemae = String;
+  nomepai = String;
+  dt_nasc = Date;
+  genero = String;
+  nacionalidade = String;
+  municipio_nascimento = String;
+  etnia = String;
+  frequenta_escola = String;
+  escolaridade = String;
+  situacao_familiar = String;
+  estabelecimento_vinculo = String;
+  estabelecimento_cadastro = String;
+
+  deficiencia = String;
+  deficiencia_visual = String;
+  deficiencia_auditiva = String;
+  deficiencia_motora = String;
+  deficiencia_intelectual = String;
+
+  dados_contato = String;
+  celular = String;
+  tel_residencial = String;
+  tel_comercial = String;
+  tel_contato = String;
+  email = String;
+
+  origem_endereco = String;
+  dados_endereco = String;
+  cpf = String;
+  identidade = String;
 
 }
